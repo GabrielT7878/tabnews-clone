5cbbe2947911f0a9cfff61b786dee940e2cba7ab
Gabriel Thiago <97263124+GabrielT7878@users.noreply.github.com>
2024-12-15 01:40:45 +0000

    add status response and tests implement

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,28 @@
 import database from "infra/database.js";
+import { version } from "react";
 
 async function status(request, response) {
-  const result = await database.query("SELECT 1 + 1 as sum;");
-  console.log(result);
-  response.status(200).json({ text: "olá estou retornando algo" });
+  const updateAt = new Date().toISOString();
+  const dbVersionResult = await database.query("SHOW server_version;");
+  const maxConnectionsResult = await database.query("SHOW max_connections;");
+  const openedConnectionsResult = await database.query(
+    "SELECT count(*)::int FROM pg_stat_activity WHERE datname = 'local_db';",
+  );
+
+  const dbVersionValue = dbVersionResult[0].server_version;
+  const maxConnectionsValue = maxConnectionsResult[0].max_connections;
+  const openedConnectionsValue = openedConnectionsResult[0].count;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependencies: {
+      database: {
+        version: dbVersionValue,
+        max_connections: parseInt(maxConnectionsValue),
+        opened_connections: openedConnectionsValue,
+      },
+    },
+  });
 }
 
 export default status;
